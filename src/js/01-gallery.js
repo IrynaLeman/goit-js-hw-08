c0057896cb6c821e8891c120ed602416957212b4
Iryna <106107121+IrynaLeman@users.noreply.github.com>
2022-10-24 17:22:47 +0300

    Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+function creareGallery(items) {
+  return items
+    .map(
+      (item) =>
+        `
+        <a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a>
+        `
+    )
+    .join('');
+}
+
+const addGallery = creareGallery(galleryItems);
+galleryEl.innerHTML = addGallery;
+//console.log(galleryEl);
+
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
