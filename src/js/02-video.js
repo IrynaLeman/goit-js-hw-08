8eea562e130045136e24fce0fa16370d3a45d7b8
Iryna <106107121+IrynaLeman@users.noreply.github.com>
2022-10-24 23:59:04 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -25,28 +25,3 @@ player.on('play', onPlay);
 
 
 
-
-
-
-/*
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
-
-const iframe = document.querySelector("iframe");
-const player = new Player(iframe);
-
-const CURRENT_TIME = "videoplayer-current-time";
-
-player.on("timeupdate", throttle(onPlay, 1000));
-
-setCurrentTime();
-
-function onPlay({ seconds }) {
-  localStorage.setItem(CURRENT_TIME, seconds);
-}
-
-function setCurrentTime() {
-  if (localStorage.getItem(CURRENT_TIME)) {
-    player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
-  }
-}*/
